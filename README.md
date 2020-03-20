57f0d3a55f693cd4a9f66ff8127fac4eb9080d54
Michael Kavanagh <michael@michaelkavanagh.me>
2020-03-20 13:55:59 +0000

    Add some packaging stuff

---
+++ README.md
@@ -0,0 +1,4 @@
+# Elexon BMRS API
+A simple wrapper for the Elexon BMRS API
+
+https://www.elexon.co.uk/guidance-note/bmrs-api-data-push-user-guide/
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="elexon-bmrs-api",
+    version="0.0.1",
+    author="Michael Kavanagh",
+    author_email="michael@michaelkavanagh.me",
+    description="A simple wrapper for the Elexon BMRS API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MichaelKavanagh/elexon-bmrs-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
