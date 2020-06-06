17a3267183a2390ddcda880a09354e8dfc0ac46e
Michael Kavanagh <michael@michaelkavanagh.me>
2020-06-06 11:20:32 +0100

    Fix stupid error and rerelease *again*

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# ensure all tests pass
+# bump version in setup.py
+# bump version in elexon/__init__.py
+
 rm dist/*
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
--- elexon/__init__.py
+++ elexon/__init__.py
@@ -1,3 +1,3 @@
-from .api import ElexonRawClient
+from .elexon import ElexonRawClient
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
