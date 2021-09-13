14ce34c64ee7e2a4fb2eadb79e0954718fdf1055
Michael Kavanagh <michael@michaelkavanagh.me>
2021-09-13 17:45:22 +0100

    Revert "Switch from setup.py to setup.cfg"
    This reverts commit 63c99c07ade5aabbbc0e98fe7a3a17b39ede8392.

--- elexon/__init__.py
+++ elexon/__init__.py
@@ -1,3 +1,3 @@
 from .elexon import ElexonRawClient
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md'), "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="elexon",
+    version="0.2.3",
+    author="Michael Kavanagh",
+    description="A simple wrapper for the Elexon BMRS API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MichaelKavanagh/elexon",
+    packages=setuptools.find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'elexon = elexon.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests']
+)
